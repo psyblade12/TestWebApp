51093d7a6a2c8969f3d9c320aad6056f58cf7f4f
Thuan Vu <ana44mag@yahoo.com>
2025-09-30 00:45:46 +0700

    commit

--- TestWebApp/Program.cs
+++ TestWebApp/Program.cs
@@ -1,11 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
@@ -19,6 +21,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapGrpcService<WeatherServiceImpl>();
+app.MapGet("/", () => "This server only supports gRPC. Use a gRPC client.");
 
 app.MapControllers();
 
---
+++ TestWebApp/ProtoServices/WeatherService.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Core;
+using MyApi.Grpc;
+
+public class WeatherServiceImpl : WeatherService.WeatherServiceBase
+{
+    public override Task<WeatherReply> GetWeather(WeatherRequest request, ServerCallContext context)
+    {
+        var forecast = $"Weather in {request.City} is sunny";
+        return Task.FromResult(new WeatherReply { Forecast = forecast });
+    }
+}
