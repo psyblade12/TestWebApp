8237264a05c10ddf4a79a52deefea5c8a74c3a68
Thuan Vu <ana44mag@yahoo.com>
2025-10-23 22:53:49 +0700

    commit

--- TestWebApp/Services/DuckDbService.cs
+++ TestWebApp/Services/DuckDbService.cs
@@ -23,16 +23,16 @@ namespace TestWebApp.Services
                 var conn = new DuckDBConnection("DataSource=:memory:");
                 conn.Open();
 
-                using var cmd = conn.CreateCommand();
+                //using var cmd = conn.CreateCommand();
 
-                cmd.CommandText = $"SET azure_storage_connection_string = '{blobStorageConnectionString}'; ";
-                cmd.ExecuteNonQuery();
+                //cmd.CommandText = $"SET azure_storage_connection_string = '{blobStorageConnectionString}'; ";
+                //cmd.ExecuteNonQuery();
 
-                cmd.CommandText = $"CREATE VIEW generated AS SELECT * FROM read_parquet('azure://tantestdatalake.blob.core.windows.net/data/generated/*.parquet');";
-                cmd.ExecuteNonQuery();
+                //cmd.CommandText = $"CREATE VIEW generated AS SELECT * FROM read_parquet('azure://tantestdatalake.blob.core.windows.net/data/generated/*.parquet');";
+                //cmd.ExecuteNonQuery();
 
-                cmd.CommandText = $"CREATE VIEW flights AS SELECT * FROM read_parquet('azure://tantestdatalake.blob.core.windows.net/data/flightdata/*.parquet');";
-                cmd.ExecuteNonQuery();
+                //cmd.CommandText = $"CREATE VIEW flights AS SELECT * FROM read_parquet('azure://tantestdatalake.blob.core.windows.net/data/flightdata/*.parquet');";
+                //cmd.ExecuteNonQuery();
 
                 _pool.Enqueue(conn);
             }
